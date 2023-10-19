5966e1e07e9dc59b2010a73e59b32757769dded1
monaon9 <102170728+monaon9@users.noreply.github.com>
2023-10-19 15:57:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/advnativew4/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.advnativew4.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.advnativew4.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
