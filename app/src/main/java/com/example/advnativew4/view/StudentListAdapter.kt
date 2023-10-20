569b9539f64d7aeda9eefc97cedb9a2f862739df
monaon9 <102170728+monaon9@users.noreply.github.com>
2023-10-20 15:25:01 +0700

    slide 41 - 52 : model

---
+++ app/src/main/java/com/example/advnativew4/view/StudentListAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.advnativew4.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.advnativew4.R
+import com.example.advnativew4.model.Student
+
+
+class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>()
+{
+    class StudentViewHolder(var view:View) : RecyclerView.ViewHolder(view){
+        val txtID:TextView
+        val txtName:TextView
+        val btnDetail:Button
+
+        init {
+            txtID = view.findViewById(R.id.txtID)
+            txtName = view.findViewById(R.id.txtName)
+            btnDetail = view.findViewById(R.id.btnDetail)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.student_list_item, parent, false)
+        return StudentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.txtID.text = studentList[position].id
+        holder.txtName.text = studentList[position].name
+        holder.btnDetail.setOnClickListener{
+            val action = StudentListFragmentDirections.actionStudentDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return studentList.size
+    }
+
+    fun updateStudentList(newStudentList: ArrayList<Student>) {
+        studentList.clear()
+        studentList.addAll(newStudentList)
+        notifyDataSetChanged()
+    }
+
+}
